1ca369602c597130cd2a74427d829a5bb7aa2f94
Micah Beech <47065407+micahbeech@users.noreply.github.com>
2023-09-19 13:45:41 -0400

    Add home about animation

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,40 @@
+import { useState, useEffect } from 'react';
 import logo from "../icons/profile-pic.jpg";
 import "../styles/Home.css";
-import "../styles/Section.css";
-import "../styles/Image.css"
+import "../styles/Section.css" ;
+import "../styles/Image.css";
 
 function Home() {
   return (
-    <section id="home">
-      <div>
-        <img src={logo} className="rounded-image" alt="Profile Picture" width="300"/>
-        <p>Home</p>
+    <section className="home">
+      <div className="about">
+        <img src={logo} className="rounded" alt="Profile" width="300"/>
+
+        <div className="content">
+          <AboutHeader/>
+
+          <p className="description">Something about me</p>
+        </div>
       </div>
     </section>
+  );
+}
+
+const identifiers = ['Big Cock Daddy', 'CS Loser', 'Cutie'];
+
+function AboutHeader() {
+  const [index, setIndex] = useState(0);
+  const [text, setText] = useState(identifiers[index]);
+
+  useEffect(() => {
+    const interval = setInterval(() => { setIndex(i => i + 1); }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => setText(identifiers[index % identifiers.length]), [index])
+
+  return (
+    <h1 key={text} className="about-header">{text}.</h1>
   );
 }
 
