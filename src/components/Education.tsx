0b330922477838e18a6f8411348bf2e0b3c58485
Micah Beech <47065407+micahbeech@users.noreply.github.com>
2023-09-19 23:30:11 -0400

    Add education section

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,7 +1,22 @@
+import '../styles/Education.css';
+import waterloo from '../icons/waterloo.png';
+
 function Education() {
   return (
     <section id="education" className="education">
-      <div>Education</div>
+      <div className="container">
+        <img src={waterloo} className="logo" alt="University of Waterloo"/>
+
+        <div className="content">
+          <h1>Bachelor of Computer Science</h1>
+
+          <p>2018 - 2023</p>
+
+          <p>Graduated with Distinction, Honours</p>
+
+          <p>Diploma in French Language I</p>
+        </div>
+      </div>
     </section>
   );
 }
