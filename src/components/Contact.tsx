72bf1735f4e30a289e7cb180d50733208e6d34b8
Micah Beech <47065407+micahbeech@users.noreply.github.com>
2023-09-20 00:15:59 -0400

    Add contacts section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,24 @@
+import '../styles/Contact.css';
+import github from '../icons/github.png';
+import linkedin from '../icons/linkedin.avif';
+import email from '../icons/email.jpeg';
+
 function Contact() {
   return (
     <section id="contact" className="contact">
-      <div>Contact</div>
+      <div className="contacts">
+        <a href="https://github.com/micahbeech">
+          <img src={github} className="contact" alt="GitHub"></img>
+        </a>
+
+        <a href="https://www.linkedin.com/in/micahbeech/">
+          <img src={linkedin} className="contact" alt="LinkedIn"></img>
+        </a>
+
+        <a href="mailto:beechmicah@gmail.com">
+          <img src={email} className="contact" alt="eMail"></img>
+        </a>
+      </div>
     </section>
   );
 }
